ad04713489e3d5450f238e29f54517937489dadd
Jose Wendel <wendelpviana@gmail.com>
2024-07-20 14:32:40 -0300

    style: adding global styles

--- src/styles/Global.styles.ts
+++ src/styles/Global.styles.ts
@@ -1,12 +1,78 @@
 import styled, { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme.styles";
 
 export const GlobalStyles = createGlobalStyle`
+/*========== Variables css ==========*/
+
+:root {
+    --header-height: 5rem;
+
+    /*========== Colors ==========*/
+    --white-color: ${theme.colors.surface.white};
+
+    --primary-color: ${theme.colors.primary.main};
+    --secondary-color: ${theme.colors.secondary.main};
+
+    --first-color-gradient-top: ${theme.gradientTop.firtsColor};
+    --second-color-gradient-top: ${theme.gradientTop.secondColor};
+
+    --first-color-gradient-bottom: ${theme.gradientBottom.firstColor};
+    --second-color-gradient-bottom:  ${theme.gradientBottom.secondColor};
+
+    --logo-color-name: ${theme.logo.name};
+
+    --bg-container-about: ${theme.colors.primary.light};
+
+    --text-color: ${theme.typography.main};
+    --text-color-light: ${theme.typography.light};
+
+    --title-color-white: var(--white-color);
+    --title-color-primary: var(--primary-color);
+    --title-color-black: var(--text-color);
+
+    --body-color: var(--white-color);
+
+    --bg-color-gradient-top: linear-gradient(90deg, var(--first-color-gradient-top) 28%, var(--second-color-gradient-top) 100%);
+    --bg-color-gradient-bottom: linear-gradient(90deg,  var(--first-color-gradient-bottom) 0%, var(--second-color-gradient-bottom) 100%);
+
+    /*========== Font and typography ==========*/
+    --body-font: 'Roboto', sans-serif;
+    --standard-font-size: 16px;
+
+    --big-font-size-logo: 50px;
+    --small-font-size-logo: 38px;
+    --smaller-font-size-logo: 10px;
+
+    --title-font-size: 40px;
+    --subtitle-font-size: 28px;
+
+    --normal-font-size-description: 22px;
+    --small-font-size-description: 20px;
+    --smaller-font-size-description: 18px;
+
+    /*========= Line height ==========*/
+    --normal-line-height: 32px;
+    --small-line-height: 28px;
+    --smaller-line-height: 24px;
+
+
+    /*========== Font weight ==========*/ 
+    --font-regular: 400;
+    --font-medium: 500;
+    --font-semi-bold: 600;
+    --font-bold: 700;
+    --font-black: 900;
+
+    /*========= Font stretch =========*/
+    --font-stretch: condensed;
+    
+}
 
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;
+    font-family: var(--body-font);
 }
 
 html, body {
@@ -25,7 +91,7 @@ a {
 
 export const Container = styled.div`
     width: 100%;
-    max-width: 1360px;
+    max-width: 1440px;
     margin: 0 auto;
-    padding: 0 26px;
+    padding: 0 80px;
 `
---
+++ src/styles/Theme.styles.ts
@@ -0,0 +1,49 @@
+
+
+export const theme = {
+
+    colors: {
+        primary: {
+            light: "#EEE9F5",
+            lightHover: "#E6DFF0",
+            main: "#56279B",
+            dark: "#411D74"
+        },
+        secondary: {
+            light: "#FDE6F6",
+            main: "#EB00A0",
+            dark: "#B00078"
+        },
+        surface: {
+            white: "#FFFFFF",
+            whiteHover: "#FAFAFA",
+            whiteActive: "#F1F2F4"
+        },
+    },
+
+    stroke: {
+        light: "#FCFCFD",
+        main: "#E5E5E6",
+        dark: "#ACACAD"
+    },
+
+    typography: {
+        light: "#ADB2B7",
+        main: "#485360",
+        dark: "#191D22"
+    },
+
+    gradientTop: {
+        firtsColor: "#7D279B",
+        secondColor: "#EB00A0"
+    },
+
+    gradientBottom: {
+        firstColor: "#EF4CC2",
+        secondColor: "#AC70FF",
+    },
+
+    logo: {
+        name: "#9300C7"
+    }
+}
