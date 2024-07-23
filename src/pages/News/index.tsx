385241b54e576679c4e59f138b4e5a26e7235964
Jose Wendel <wendelpviana@gmail.com>
2024-07-23 12:27:42 -0300

    :sparkles: feat: insert banner news

--- src/pages/News/index.tsx
+++ src/pages/News/index.tsx
@@ -1,3 +1,11 @@
+import { BannerNews } from "./styles";
+
 export function News() {
-  return <h1>Página de Notícias</h1>;
+  return (
+    <>
+      <BannerNews>
+        <h2>Notícias | Projeto Meninas de Sistemas</h2>
+      </BannerNews>
+    </>
+  );
 }
---
+++ src/pages/News/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BannerNews = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 300px;
+  background: var(--bg-color-gradient-top);
+  color: var(--white-color);
+`;
